fixed rotation axis in DBF & added code to plot directions

codes/DBF.py
@@ -17,46 +17,40 @@ DBF_FILENAME    = sys.argv[5]
 dirs = load_directions(DIRS_FILENAME)
 ndirs = dirs.shape[0]
 
-dwi = nib.load(DWI_FILENAME)
-g,b,idx = load_scheme(SCHEME_FILENAME)
-s = dwi.get_fdata()
-mask = nib.load( MASK_FILENAME ).get_fdata()
+dwi  = nib.load(DWI_FILENAME)
+mask = nib.load(MASK_FILENAME).get_fdata()
+g,b,_ = load_scheme(SCHEME_FILENAME)
+S = dwi.get_fdata()
 
-xdim,ydim,zdim,N = dwi.shape[0], dwi.shape[1], dwi.shape[2], dwi.shape[3]
-voxels = itertools.product( range(xdim), range(ydim), range(zdim) )
+X,Y,Z,N = S.shape
+voxels = itertools.product( range(X), range(Y), range(Z) )
 
-lambdas = np.array([0.0003,0.0003,0.004])
+lambdas = np.array([0.0003,0.0003,0.015])
 
-PHI = np.zeros((N,ndirs), dtype=np.float32)
-S = np.zeros(N, dtype=np.float32)
-S_hat = np.zeros((xdim,ydim,zdim,N), dtype=np.float32)
-alphas = np.zeros((xdim,ydim,zdim,ndirs), dtype=np.float32)
+design_matrix = np.zeros((N,ndirs), dtype=np.float32)
+S_hat = np.zeros((X,Y,Z,N), dtype=np.float32)
+alphas = np.zeros((X,Y,Z,ndirs), dtype=np.float32)
+bases = np.zeros((X,Y,Z,6), dtype=np.float64)
 
 for i in range(N):
     for j in range(ndirs):
-        Rj = getRotationFromDir( (0,1,0), dirs[j])
+        Rj = getRotationFromDir( (0,0,1), dirs[j])
         Tj = Rj.transpose() @ np.diag(lambdas) @ Rj
-        PHI[i,j] = np.exp( -b[i]*(g[i].transpose()@Tj@g[i]) )
+        design_matrix[i,j] = np.exp( -b[i]*(g[i].transpose()@Tj@g[i]) )
 
 for (x,y,z) in voxels:
     if mask[x,y,z]:
-        S = s[x,y,z, :]
-        #alpha = np.linalg.lstsq(PHI, S) [0] # usar non-negative least squaresO
-        alpha, res = nnls(PHI, S)
-        S_hat[x,y,z, :] = PHI@alpha
-        alphas[x,y,z, :] = alpha
+        #alpha = np.linalg.lstsq(design_matrix, S) [0] # usar non-negative least squaresO
+        sol, res = nnls(design_matrix, S[x,y,z, :])
+
+        T = np.diag(lambdas)
+        bases[x,y,z, :] = np.array([ T[0,0], T[1,1], T[2,2], T[0,1], T[0,2], T[1,2] ])
+        alphas[x,y,z, :] = sol
+        S_hat[x,y,z, :] = design_matrix@sol
 
 #mandar volumen de alphas con las direccciones a mrtrix
-nib.save( nib.Nifti1Image(alphas , dwi.affine, dwi.header), 'alphas.nii' )
+nib.save( nib.Nifti1Image(alphas , dwi.affine, dwi.header), 'dbf_alphas.nii' )
+nib.save( nib.Nifti1Image(bases , dwi.affine, dwi.header), 'dbf_base.nii' )
 nib.save( nib.Nifti1Image(S_hat , dwi.affine, dwi.header), DBF_FILENAME )
 
-fig = plt.figure(figsize=(4,4))
 
-ax = fig.add_subplot(111, projection='3d')
-
-ax.plot(dirs[:,0], dirs[:,1], dirs[:,2], linewidth=0, marker='o', markersize=2)
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-
-plt.show()#"""